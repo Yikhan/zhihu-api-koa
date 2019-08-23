df28b9bfb3420d6a340ffeef9a616c6dd2e71a66
Yikhan <yhxiaoch@gmail.com>
2019-08-23 20:58:56 +1000

    simple route implementation

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const Koa = require('koa')
 const app = new Koa()
 
 app.use(async (ctx, next) => {
-    await next()
-    console.log('First middleware')
-    ctx.body = 'Hello World, Haha'
-})
-
-app.use(async (ctx) => {
-    console.log('Second middleware')
+    if (ctx.url === '/') {
+        ctx.body = 'main page'
+    } else if (ctx.url === '/users') {
+        if (ctx.method === 'GET') {
+            ctx.body = 'user list'
+        } else if (ctx.method === 'POST') {
+            ctx.body = 'create user'
+        } else {
+            //method not allowed
+            ctx.status = 405 
+        }
+    } else if (ctx.url.match(/\/users\/\w+/)) {
+        // parameter extraction
+        const userId = ctx.url.match(/\/users\/(\w+)/)[1]
+        ctx.body = `this is user: ${userId}`
+    } else {
+        ctx.status = 404
+    }
 })
 
 // 监听端口
